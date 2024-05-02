9bb46399535baa0a4e84c09af71fe2b3d2849c31
Dot8Pixels <napatsriz@gmail.com>
2024-05-02 23:36:17 +0700

    feat: :sparkles: Control flow

--- go_basic/main.go
+++ go_basic/main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// n := 3
+
+	// if n%2 == 0 {
+	// 	fmt.Println("Even")
+	// } else {
+	// 	fmt.Println("Odd")
+	// }
+
+	// score := 65
+
+	// if score >= 80 {
+	// 	fmt.Println("A")
+	// } else if score >= 70 {
+	// 	fmt.Println("B")
+	// } else if score >= 60 {
+	// 	fmt.Println("C")
+	// } else if score >= 50 {
+	// 	fmt.Println("D")
+	// } else {
+	// 	fmt.Println("F")
+	// }
+
+	if a := 10 > 20; a {
+		fmt.Println("10 is greater than 20")
+	} else {
+		fmt.Println("10 is less than 20")
+	}
 }
