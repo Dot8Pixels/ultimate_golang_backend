31191fd6855d4f2d917492b324e553533ae1caa7
Dot8Pixels <napatsriz@gmail.com>
2024-05-02 23:30:35 +0700

    feat: :sparkles: Loop

--- go_basic/main.go
+++ go_basic/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	for i := 1; i <= 3; i++ {
+		fmt.Println(i)
+	}
+
+	var sum int
+	for i := 1; i <= 10; i++ {
+		sum += 1
+		fmt.Println(sum)
+	}
+
+	for i := range 3 {
+		fmt.Println(i + 1)
+	}
 }
