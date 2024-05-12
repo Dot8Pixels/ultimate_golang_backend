24803b106f4931ef5b00a54dcd615c8428eeaa4f
Dot8Pixels <napatsriz@gmail.com>
2024-05-12 23:21:35 +0700

    feat: :sparkles: Pointers

--- go_basic/main.go
+++ go_basic/main.go
@@ -2,6 +2,54 @@ package main
 
 import "fmt"
 
+func double(n *int) {
+	*n *= 2
+}
+
 func main() {
-	fmt.Println("Hello World")
+	// b := new(int)
+	// c := new(*int)
+
+	// a := 1
+	// b = &a
+	// c = &b
+
+	// fmt.Printf("address a %v\n", &a)
+	// fmt.Printf("address b %v\n", &b)
+	// fmt.Printf("address c %v\n", &c)
+
+	// fmt.Println()
+
+	// fmt.Printf("value a %v\n", a)
+	// fmt.Printf("value b %v\n", b)
+	// fmt.Printf("value c %v\n", c)
+
+	// fmt.Println()
+
+	// fmt.Printf("*b %v\n", *b)
+	// fmt.Printf("*c %v\n", *c)
+	// fmt.Printf("**c %v\n", **c)
+
+	// fmt.Println()
+
+	// x := 2
+	// var n *int = &x
+
+	// println(x)
+	// println(n)
+
+	// double(n)
+
+	// fmt.Println(*n)
+	// fmt.Println(x)
+
+	// fmt.Println()
+
+	y := 2
+
+	println(y)
+
+	double(&y)
+
+	fmt.Println(y)
 }
